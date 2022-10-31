07d1fb186cdd8aad0c3b24d5873f37f0a1325b7f
Marouane Souda <marsou431@gmail.com>
2022-10-31 16:00:06 +0100

    refactor: remove testimonials data into its own file

--- components/Testimonials/Testimonials.tsx
+++ components/Testimonials/Testimonials.tsx
@@ -1,10 +1,10 @@
 import styled from "styled-components";
-import { Testimonial as TTestimonial, Theme } from '../../types';
 import Title from "../../shared/ui/Title";
 import Testimonial from "./Testimonial/Testimonial";
 import CallToAction from "../../shared/ui/CallToAction";
 import { Splide, SplideSlide, Options } from '@splidejs/react-splide';
 import '@splidejs/react-splide/dist/css/splide.min.css';
+import testimonials from "./data";
 
 const Container = styled.section`
   text-align: center;
@@ -12,37 +12,10 @@ const Container = styled.section`
   &::-webkit-scrollbar {
     background: transparent !important;
     width: 0 !important;
-  }           
+  }
   scrollbar-color: transparent transparent !important;
   scrollbar-width: none !important;
 `;
-
-const testimonials: TTestimonial[] = [
-  {
-    slide: 1,
-    image: '/images/avatar-anisha.png',
-    name: 'Anisha Li',
-    quote: 'Manage has supercharged our team’s workflow. The ability to maintain visibility on larger milestones at all times keeps everyone motivated.',
-  },
-  {
-    slide: 2,
-    image: '/images/avatar-ali.png',
-    name: 'Ali Bravo',
-    quote: 'We have been able to cancel so many other subsriptions since using Manage. There is no more cross-channel confusion and everyone is much more focused.',
-  },
-  {
-    slide: 3,
-    image: '/images/avatar-richard.png',
-    name: 'Richard Watts',
-    quote: 'Manage allows us to provide structure and process. It keeps us organized and focused. I can\’t stop recommending them to everyone I talk to!',
-  },
-  {
-    slide: 4,
-    image: '/images/avatar-shanai.png',
-    name: 'Shanai Gough',
-    quote: 'Their software allows us to track, manage and collaborate on our projects from anywhere. It keeps the whole team in-sync without being intrusive.',
-  },
-];
 
 function Testimonials() {
   const options: Options = {
@@ -65,19 +38,16 @@ function Testimonials() {
   return (
     <Container>
       <Title>What they've said</Title>
-
       
       <Splide options={options} >
         {testimonials.map(testimonial => {
           return (
             <SplideSlide key={testimonial.name} className="slide">
-              <Testimonial slide={testimonial.slide}  image={testimonial.image} name={testimonial.name} quote={testimonial.quote} />
+              <Testimonial slide={testimonial.slide} image={testimonial.image} name={testimonial.name} quote={testimonial.quote} />
             </SplideSlide>
           );
         })}
       </Splide>
-      
-      
 
       <CallToAction>Get Started</CallToAction>
     </Container>
---
+++ components/Testimonials/data.ts
@@ -0,0 +1,30 @@
+import { Testimonial } from "../../types";
+
+const testimonials: Testimonial[] = [
+  {
+    slide: 1,
+    image: '/images/avatar-anisha.png',
+    name: 'Anisha Li',
+    quote: 'Manage has supercharged our team’s workflow. The ability to maintain visibility on larger milestones at all times keeps everyone motivated.',
+  },
+  {
+    slide: 2,
+    image: '/images/avatar-ali.png',
+    name: 'Ali Bravo',
+    quote: 'We have been able to cancel so many other subsriptions since using Manage. There is no more cross-channel confusion and everyone is much more focused.',
+  },
+  {
+    slide: 3,
+    image: '/images/avatar-richard.png',
+    name: 'Richard Watts',
+    quote: 'Manage allows us to provide structure and process. It keeps us organized and focused. I can\’t stop recommending them to everyone I talk to!',
+  },
+  {
+    slide: 4,
+    image: '/images/avatar-shanai.png',
+    name: 'Shanai Gough',
+    quote: 'Their software allows us to track, manage and collaborate on our projects from anywhere. It keeps the whole team in-sync without being intrusive.',
+  },
+];
+
+export default testimonials;
